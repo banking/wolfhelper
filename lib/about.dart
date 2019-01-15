99a11e4b9a1c5ccfcbca1f7ed472afd3cc1c83a0
Zhang Yinhang <banking17173@126.com>
2019-01-15 20:33:54 +0800

    about widget

--- lib/about.dart
+++ lib/about.dart
@@ -3,16 +3,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class About extends StatefulWidget {
+class About extends StatelessWidget {
   const About({ Key key, this.seed }) : super(key: key);
 
   final int seed;
 
   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AboutPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({ Key key, this.title }) : super(key: key);
+
+  final String title;
+
+  @override
   _AboutState createState() => _AboutState();
 }
 
-class _AboutState extends State<About> {
+class _AboutState extends State<AboutPage> {
 
   @override
   void initState() {
@@ -26,12 +43,44 @@ class _AboutState extends State<About> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple,
-      height: 200.0,
-      child: const SafeArea(
-        bottom: false,
-        child: Placeholder(),
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/main_bg.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child : Container(
+              height: 100,
+              width: 200,
+              child : Column(
+                children: <Widget>[
+                  Text(
+                    '作者：banking.',
+                    textDirection: TextDirection.ltr,
+                  ),
+                  Text(
+                    'Written by flutter.',
+                    textDirection: TextDirection.ltr,
+                  ),
+                  Text(
+                    '   ',
+                    textDirection: TextDirection.ltr,
+                  ),
+                  Text(
+                    '联系我&支付宝赞赏：',
+                    textDirection: TextDirection.ltr,
+                  ),
+                  Text(
+                    'banking17173@126.com',
+                    textDirection: TextDirection.ltr,
+                  ),
+                ],
+              ),
+            ),
+          )
       ),
     );
   }
